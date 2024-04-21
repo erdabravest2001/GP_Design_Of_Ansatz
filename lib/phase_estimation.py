19149bfde89caef355a07e528a95f138b7d7d8d5
Ethan Elliot Rajkumar <61441923+erdabravest2001@users.noreply.github.com>
2024-04-21 06:55:04 -0700

    added likelihood generation with parameters

--- lib/phase_estimation.py
+++ lib/phase_estimation.py
@@ -1,5 +1,5 @@
 
-
+import pennylane as qml
 
 def create_hamiltonians(list_of_spins, list_of_params, mol_params):
     from lib.qpe import QPE
@@ -33,18 +33,40 @@ def create_hamiltonians(list_of_spins, list_of_params, mol_params):
     return hamiltonians
     
 def convert_to_unitary(matrix, n, wire_order, time=1.0):
-    import pennylane as qml
     unitary = qml.ApproxTimeEvolution(matrix, time, n)
     U_matrix = qml.matrix(unitary,wire_order)
     return U_matrix
 
-
 def find_true_phases(U):
-   import numpy as np
-   eigvals, eigvecs = np.linalg.eig(U)
-   phi_ind = 0
-   truephi = np.angle(eigvals[phi_ind])
-   return truephi
+    import numpy as np
+    eigvals, _ = np.linalg.eig(U)
+    true_phases = np.angle(eigvals)
+    true_phases = np.mod(true_phases, 2*np.pi)
+    return true_phases
+
+def init_custom_gates(unitary_matrices):
+    custom_gates = {}
+    for index, unitary_matrix in enumerate(unitary_matrices, start=1):
+        gate_name = f"uffsg{index}"
+        custom_gates[gate_name] = qml.QubitUnitary(unitary_matrix, wires=[0, 1, 2, 3, 4])
+    return custom_gates
+
+def experiment_probs(k, beta, wires, unitary_matrices, estimation_wires, gate_index):
+    import pennylane as qml
+    from pennylane import numpy as np
+    dev = qml.device('default.qubit', wires=wires)
+    custom_gates = init_custom_gates(unitary_matrices)
+    @qml.qnode(dev)
+    def circuit(k, beta):
+        qml.broadcast(qml.Hadamard, wires=range(wires), pattern="single")
+        qml.RZ(beta, wires=0)  
+        selected_gate = custom_gates[f"uffsg{gate_index}"]
+        qml.ctrl(selected_gate, control=estimation_wires)
+        qml.broadcast(qml.Hadamard, wires=range(wires), pattern="single")
+        return qml.probs(wires=estimation_wires)
+    probs = circuit(k, beta)
+    return probs
+
 
 
     
